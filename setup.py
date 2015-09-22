156321dedbac3f7debfcce197bbf34eca44a86d2
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2015-09-22 10:13:58 +0200

    tests: fix incompatibility with pytest>=2.8.0
    * FIX Removes calls to PluginManager
      consider_setuptools_entrypoints() removed in PyTest 2.8.0.
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- setup.py
+++ setup.py
@@ -42,10 +42,10 @@ requirements = [
 ]
 
 test_requirements = [
-    'pytest>=2.6.4',
-    'pytest-cov>=1.8.0',
+    'pytest>=2.8.0',
+    'pytest-cov>=2.1.0',
     'pytest-pep8>=1.0.6',
-    'coverage>=3.7.1',
+    'coverage>=4.0.0',
 ]
 
 
@@ -77,9 +77,6 @@ class PyTest(TestCommand):
         """Run tests."""
         # import here, cause outside the eggs aren't loaded
         import pytest
-        import _pytest.config
-        pm = _pytest.config.get_plugin_manager()
-        pm.consider_setuptools_entrypoints()
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
