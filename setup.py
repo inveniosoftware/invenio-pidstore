d4d24d0c2c6b7988280b1485fe1c231a65d78731
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-08-15 18:22:37 +0200

    tests: integration of pytest-runner
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ history = open('CHANGES.rst').read()
 tests_require = [
     'check-manifest>=0.25',
     'coverage>=4.0',
-    'Flask-CLI>=0.2.1',
     'invenio-records>=1.0.0a2',
     'isort>=4.2.2',
     'mock>=1.3.0',
@@ -78,6 +77,7 @@ for name, reqs in extras_require.items():
 
 setup_requires = [
     'Babel>=1.3',
+    'pytest-runner>=2.7.1',
 ]
 
 install_requires = [
@@ -86,40 +86,6 @@ install_requires = [
 ]
 
 packages = find_packages()
-
-
-class PyTest(TestCommand):
-    """PyTest Test."""
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        """Init pytest."""
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-        try:
-            from ConfigParser import ConfigParser
-        except ImportError:
-            from configparser import ConfigParser
-        config = ConfigParser()
-        config.read('pytest.ini')
-        self.pytest_args = config.get('pytest', 'addopts').split(' ')
-
-    def finalize_options(self):
-        """Finalize pytest."""
-        TestCommand.finalize_options(self)
-        if hasattr(self, '_test_args'):
-            self.test_suite = ''
-        else:
-            self.test_args = []
-            self.test_suite = True
-
-    def run_tests(self):
-        """Run tests."""
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 # Get the version string. Cannot be done with import!
 g = {}
@@ -181,5 +147,4 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Development Status :: 3 - Alpha',
     ],
-    cmdclass={'test': PyTest},
 )
