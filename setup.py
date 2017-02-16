9bcf6e81651651cf401a999c13e4450f49bbd851
PaulinaLach <paulina.malgorzata.lach@cern.ch>
2017-02-16 14:08:52 +0100

    global: release checklist
    * FIX Adds changes according to release preparation checklist.
    
    Signed-off-by: Paulina Lach <paulina.malgorzata.lach@cern.ch>

--- setup.py
+++ setup.py
@@ -25,10 +25,8 @@
 """Invenio module that stores and registers persistent identifiers."""
 
 import os
-import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
 
 readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
